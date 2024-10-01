6c98bf88cbb66ac6b1a585bdc384259fdf6dfca8
Bruno <brunohenriquehns@gmail.com>
2024-10-01 09:46:17 -0300

    Return pagination

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,25 @@
+// import { Component } from '@angular/core';
+// import { data, type ArticleProps } from 'src/assets/data/data';
+// import { formatTitlePost } from 'src/app/utils/formatTitlePost';
+// import { sortByDate } from 'src/app/utils/sortByDate';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+// })
+// export class HomeComponent {
+//   formatTitlePost = formatTitlePost;
+
+//   database: ArticleProps[] = [];
+
+//   constructor() {
+//     if (!data) {
+//       return;
+//     }
+
+//     this.database = sortByDate(data);
+//   }
+// }
 import { Component } from '@angular/core';
 import { data, type ArticleProps } from 'src/assets/data/data';
 import { formatTitlePost } from 'src/app/utils/formatTitlePost';
@@ -11,6 +33,11 @@ export class HomeComponent {
   formatTitlePost = formatTitlePost;
 
   database: ArticleProps[] = [];
+  paginatedData: ArticleProps[] = [];
+
+  itemsPerPage = 3;
+  currentPage = 1;
+  totalPages = 1;
 
   constructor() {
     if (!data) {
@@ -18,5 +45,39 @@ export class HomeComponent {
     }
 
     this.database = sortByDate(data);
+
+    this.totalPages = Math.ceil((this.database.length - 1) / this.itemsPerPage);
+
+    this.paginateData();
+  }
+
+  paginateData() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage + 1;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedData = this.database.slice(startIndex, endIndex);
+  }
+
+  goToPage(page: number) {
+    if (page < 1 || page > this.totalPages) {
+      return;
+    }
+    this.currentPage = page;
+    this.paginateData();
+  }
+
+  nextPage() {
+    this.goToPage(this.currentPage + 1);
+  }
+
+  prevPage() {
+    this.goToPage(this.currentPage - 1);
+  }
+
+  firstPage() {
+    this.goToPage(1);
+  }
+
+  lastPage() {
+    this.goToPage(this.totalPages);
   }
 }
